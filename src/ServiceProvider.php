133d1dc3afe05e208c3d9d7f9313f2636f2cdff2
Zach Chisholm <zach@artifishall.com>
2019-01-31 14:26:57 -0600

    version 2

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -15,13 +15,13 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         $this->loadViewsFrom(__DIR__.'/views', 'bootstrap_forms');
 
-        Form::component('bsText', 'bootstrap_forms::.group', ['name', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'text']);
-        Form::component('bsTime', 'bootstrap_forms::.group', ['name', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'time']);
-        Form::component('bsDate', 'bootstrap_forms::.group', ['name', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'date']);
-        Form::component('bsNumber', 'bootstrap_forms::.group', ['name', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'number']);
-        Form::component('bsTextarea', 'bootstrap_forms::.group', ['name', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'textarea']);
-        Form::component('bsSelect', 'bootstrap_forms::.group', ['name', 'options', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'select']);
-        Form::component('bsRadio', 'bootstrap_forms::.group', ['name', 'options', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'radio']);
+        Form::component('bsText', 'bootstrap_forms::group', ['name', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'text']);
+        Form::component('bsTime', 'bootstrap_forms::group', ['name', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'time']);
+        Form::component('bsDate', 'bootstrap_forms::group', ['name', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'date']);
+        Form::component('bsNumber', 'bootstrap_forms::group', ['name', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'number']);
+        Form::component('bsTextarea', 'bootstrap_forms::group', ['name', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'textarea']);
+        Form::component('bsSelect', 'bootstrap_forms::group', ['name', 'options', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'select']);
+        Form::component('bsRadio', 'bootstrap_forms::group', ['name', 'options', 'value' => null, 'title' => null, 'attributes' => [], 'extras' => null, 'type' => 'radio']);
 
         Form::component('bsSubmit', 'bootstrap_forms::submit', ['name', 'style' => 'primary', 'value' => null, 'attributes' => []]);
         Form::component('bsButton', 'bootstrap_forms::button', ['name', 'style' => 'primary', 'value' => null, 'attributes' => []]);
